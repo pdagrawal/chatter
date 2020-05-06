8150c8d2f1415841bf5b34845c470b375ad8640e
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-05-07 04:19:33 +0530

    create chat message changes

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -15,6 +15,7 @@ class ChatsController < ApplicationController
   # GET /chats/new
   def new
     @chat = Chat.new
+    @chats =Chat.all
   end
 
   # GET /chats/1/edit
@@ -28,8 +29,9 @@ class ChatsController < ApplicationController
 
     respond_to do |format|
       if @chat.save
-        format.html { redirect_to @chat, notice: 'Chat was successfully created.' }
-        format.json { render :show, status: :created, location: @chat }
+        # format.html { redirect_to @chat, notice: 'Chat was successfully created.' }
+        # format.json { render :show, status: :created, location: @chat }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @chat.errors, status: :unprocessable_entity }
